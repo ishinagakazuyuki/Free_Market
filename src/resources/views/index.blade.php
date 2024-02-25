8cf6f681da9273755086db519c512c54ac28a3d6
ishinaga kazuyuki <ishikazu0920@gmail.com>
2024-02-26 03:56:37 +0900

    トップページの画像表示間隔の修正

--- src/resources/views/index.blade.php
+++ src/resources/views/index.blade.php
@@ -25,7 +25,11 @@
                     </button>
                 </form>
             </div>
+            <?php $count = $count + 1; ?>
             @endforeach
+            <?php $number = $count % 5; ?>
+            <div class="tab-box{{ $number }}"></div>
+            @endif
         </div>
         <div id="tab2" class="tab-content" style="display: none;">
             @if ($mylist === null)
